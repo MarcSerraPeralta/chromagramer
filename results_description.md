eb6ace0a693155a4b3fa12ab2d0012950c4faeb7
MarcSerraPeralta <43704266+MarcSerraPeralta@users.noreply.github.com>
2021-10-14 10:13:35 +0200

    Added description of the results

---
+++ results_description.md
@@ -0,0 +1,52 @@
+## Text files folder (`txt_kunstderfuge` by default)
+
+It has the same tree structure as input folder (_Kunstderfuge_ corpus' folder by default) and its files have the same names as the input ones. 
+The text files are the output files of `midi2abc`. 
+
+## Continous chromagram folder (`chr_kunstderfuge` by default)
+
+It has the same tree structure as input folder (_Kunstderfuge_ corpus' folder by default) and its files have the same names as the input ones. 
+The structure of each file is:
+1. First line: time in which the piece changes its key signature (`.2f` separated by spaces).
+1. Second line: time and bar of the different parts of the piece (`.2f X/Y` separated by two spaces).
+1. Rest of the lines: continous chromagran with chroma duration specified by `Atmin` in `lib/txt2input.py` (12 `.2f` separated by spaces, each line).
+
+## `zipf_authors` folder
+
+It contains the Zipf information of each author in the dataset (aggregated author's pieces). 
+Each row of the file has the token and types of each codeword (sorted descending):
+```
+100010010000 1659     # codeword 100010010000 appears with absolute frequency = 1659
+000000000000 1461
+100001000100 1157
+(...)
+```
+
+> :bookmark: `zipf2note` function in [`lib/extra.py`](lib/extra.py) translates binary codewords to chords
+>
+> example: "010010000100" ==> ['C#', 'E', 'A'] 
+
+## `list_short_pieces.txt` file
+
+It contains the full path and name of the pieces with `L <= Lmin` (see [`script.py`](script.py))
+
+## `zipf_corpus_(...).txt` file (`zipf_corpus_transposed_1-00_0-1000.txt` by default)
+
+It is the aggregation of all pieces in the input folder, with the same structure as the files in `zipf_authors` folder. 
+
+## `LV_authors_(...).txt` and `LV_pieces_(...).txt` files (`LV_authors_transposed_1-00_0-1000` and `LV_pieces_transposed_1-00_0-1000` by default)
+
+Both files contain the following information for each author/piece: `author L V`:
+```
+albeniz 42313 1666
+albinoni 29245 711
+albrechtsberger 4760 511
+```
+
+## `zipf_authors_(...).csv` files 
+
+It contains a table with the parameters of the maximum likelihood fit of each author, as well as other important variables. 
+
+## `best_distribution_authors.csv` file
+
+It contains the best fit of each author following the criteria of [`select_best_distribution.py`](lib/select_best_distribution.py). 
--- script.py
+++ script.py
@@ -1,12 +1,7 @@
 import os
 
 from lib import main
-
-from lib import extra
-from lib import input2LVzipf
-from lib import LV2richness
-from lib import mid2txt
-from lib import txt2input
+from lib import extra, input2LVzipf, LV2richness, mid2txt, txt2input
 from lib.prepare_kunstderfuge import prepare_corpus
 
 ##################################################################
@@ -14,60 +9,60 @@ from lib.prepare_kunstderfuge import prepare_corpus
 # directory that contains all the data to analyse
 corpus_folder = "midi_kunstderfuge"
 
+# file with list of songs to be deleted because they are repeated
+repeated_songs_file = "list_repeated_pieces.txt"
+
+# minimum lenght (in beats) that songs must have (deletes songs with L <= Lmin)
+Lmin = 15
+
 # output folders
 output_txt = "txt_kunstderfuge"
 output_chr = "chr_kunstderfuge"
 
 ##################################################################
 
-# PREPARE CORPUS
-print("PREPARING CORPUS...")
-prepare_corpus(corpus_folder)
-print("Done")
+# PREPARE CORPUS 
+# Deletes miscellaneous folders, files that are not MIDIs, repeated songs
+print("STARTING PREPARING CORPUS...")
+prepare_corpus(corpus_folder, repeated_songs_file=repeated_songs_file)
+print("PREPARING CORPUS DONE")
 
 # GENERATE TXT FILES FROM MIDI FILES
-print("\nGENERATING TXT FILES...")
+print("\nSTARTING GENERATING TXT FILES...")
 main.database(corpus_folder, mid2txt.mid2txt, OUTDIR=output_txt) 
-print("Done")
+print("GENERATING TXT FILES DONE")
 # calculate txt files
 n_files = sum([len(files) for r, d, files in os.walk("dat/" + output_txt)])
-print("Total number of txt files in corpus:", n_files, "(should be 10523)")
+print("Total number of txt files in corpus:", n_files)
 print("(midi files that are not corrupted and can be translated to txt)")
 
 # GENERATE CONTINUOUS CHROMAS
-print("\nGENERATING CONTINUOUS CHROMAS...")
+print("\nSTARTING GENERATING CONTINUOUS CHROMAS...")
 main.database(output_txt, txt2input.txt2input, OUTDIR=output_chr, POSTFUNCT=txt2input.post_txt2input)
-print("Done")
+print("GENERATING CONTINUOUS CHROMAS DONE")
 # calculate txt files
 n_files = sum([len(files) for r, d, files in os.walk("dat/" + output_chr)])
-print("Total number of chr files in corpus:", n_files, "(should be 9564)")
+print("Total number of chr files in corpus:", n_files)
 print("(txt files that fulfill all the requirements (bar, not-empty,...))")
 
+# DETELE PIECES SHORTER THAN Lmin (L <= Lmin)
+main.database(output_chr, extra.get_short_pieces, ARGS = [Lmin, "list_short_pieces.txt"], POSTFUNCT=extra.post_get_short_pieces)
+f = open("dat/list_short_pieces.txt", "r")
+short_pieces = f.read()
+f.close()
+for name in [i for i in short_pieces.split("\n") if i != ""]:
+	os.remove("dat/" + name)
+# calculate txt files
+n_files = sum([len(files) for r, d, files in os.walk("dat/" + output_chr)])
+print("Total number of chr files in corpus:", n_files)
+print("(chr files that fulfill all the requirements (not-short, bar, not-empty,...))")
+
 # CALCULATE L,V,zipf_authors
-print("\nCALCULATING L,V,ZIPF_AUTHORS...")
+print("\nSTARTING CALCULATING L,V,ZIPF_AUTHORS...")
 main.database(output_chr, input2LVzipf.input2LVzipf, OUTDIR="tmp", ARGS=[0.25, 1, 0.1, "transposed"], 
 	PREFUNCT=input2LVzipf.pre_input2LVzipf, POSTFUNCT=input2LVzipf.post_input2LVzipf)
-print("Done")
+print("CALCULATING L,V,ZIPF_AUTHORS DONE")
 # calculate Ltotal, Vtotal corpus
 Ltotal = extra.get_Ltotal("zipf_corpus_transposed_1-00_0-1000.txt", PRINT=False)
 Vtotal = extra.get_Vtotal("zipf_corpus_transposed_1-00_0-1000.txt", PRINT=False)
-print("Total L and V of corpus: L={} V={}".format(Ltotal, Vtotal), "(should be 5152589 and 4085)")
-
-# LINEAR REGRESSION logV vs logL
-print("\nLINEAR REGRESSION logV vs logL")
-extra.LV_regression(["LV_authors_transposed_1-00_0-1000.txt","LV_pieces_transposed_1-00_0-1000.txt"])
-print("Done")
-
-# CREATE TABLE LV AND HISTOGRAM OF KEYS
-print("\nCREATE TABLE LV...")
-extra.LV_table(1, 0.1)
-print("Done")
-print("\nPLOTS HISTOGRAM OF KEYS...")
-main.database(output_chr, extra.get_hist_keys, ARGS=[0.25, 1, 0.1], POSTFUNCT=extra.post_get_hist_keys)
-extra.plot_hist_keys()
-print("Done")
-
-# CREATE TABLE RICHNESS
-print("\nCREATE TABLE RICHNESS...")
-LV2richness.LV2richness(1, 0.1)
-print("Done")
+print("Total L and V of corpus: L={} V={}".format(Ltotal, Vtotal))
